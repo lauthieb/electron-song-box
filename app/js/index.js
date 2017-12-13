0ab896fd3af4429650269552b5dc3e8d34cba292
Laurent THIEBAULT <external.z05lthie@decathlon.com>
2017-12-13 21:16:33 +0100

    feat(songs): Play songs on click

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,21 @@
-// Add your index.js code in this file
+'use strict';
+
+
+const soundButtons = document.querySelectorAll('.button-sound');
+
+for (let i = 0; i < soundButtons.length; i++) {
+    const soundButton = soundButtons[i];
+    const soundName = soundButton.attributes['data-sound'].value;
+
+    prepareButton(soundButton, soundName);
+}
+
+function prepareButton(buttonEl, soundName) {
+    buttonEl.querySelector('span').style.backgroundImage = 'url("img/icons/' + soundName + '.png")';
+
+    const audio = new Audio(__dirname + '/wav/' + soundName + '.wav');
+    buttonEl.addEventListener('click', () => {
+        audio.currentTime = 0;
+        audio.play();
+    });
+}

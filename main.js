ff55216b19010206e6a56ff49dfc27b7f6b62447
Laurent THIEBAULT <external.z05lthie@decathlon.com>
2017-12-13 20:16:03 +0100

    init(project): Start project

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 'use strict';
 
-let app = require('app');
-let BrowserWindow = require('browser-window');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+let mainWindow;
 
-let mainWindow = null;
-
-app.on('ready', function() {
+function createWindow () {
     mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800
+        frame: false,
+        height: 610,
+        resizable: false,
+        width: 368
     });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, '/app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
 });
